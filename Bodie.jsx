e168cd6cd5a587cafff0e684c412fadafc2e254d
DeepikaDaran24 <85444102+DeepikaDaran24@users.noreply.github.com>
2021-06-06 21:34:44 +0530

    Add files via upload

---
+++ Bodie.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Bodie(){
+    
+    return(
+        <div  className="bodie">
+            <h1>CONTENT</h1>
+            <p>
+            The domestic dog is a domesticated descendant of the wolf. The dog derived from an ancient, extinct wolf,and the modern grey wolf is the dog's nearest living relative
+            </p>
+        </div>
+
+    );
+}
+export default Bodie;
